7ca7cc3c468f81df9b6ff99c27a63b8efb4f00a9
Max Dehaut <mdehaut@live.com>
2022-03-24 15:43:59 +0100

    initial commit

---
+++ cadai/Binning.py
@@ -0,0 +1,70 @@
+#  Weight Of Evidence
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import seaborn as sns
+
+from seaborn import axes_style
+
+
+def woe_eval(df, feature_name, y_df, bins=2):
+    if (feature_name in df.select_dtypes(include='number').columns):
+        bin_feature_name = feature_name + '_bin'
+        df[bin_feature_name] = pd.cut(df[feature_name], bins)
+        feature_name = bin_feature_name
+
+    df = pd.concat([df[feature_name], y_df], axis=1)
+    df = pd.concat([df.groupby(df.columns.values[0], as_index=False)[df.columns.values[1]].count(),
+                    df.groupby(df.columns.values[0], as_index=False)[df.columns.values[1]].mean()],axis=1)
+    df = df.iloc[:, [0, 1, 3]]
+    df.columns = [df.columns.values[0], 'nbr_obs', 'prob_event']
+    df['prob_obs'] = df['nbr_obs'] / df['nbr_obs'].sum()
+    df['nbr_event'] = df['prob_event'] * df['nbr_obs']
+    df['nbr_event'] = df['nbr_event'].astype(int)
+
+    df['nbr_nevent'] = (1 - df['prob_event']) * df['nbr_obs']
+    df['nbr_nevent'] = df['nbr_nevent'].astype(int)
+
+    df['prob_event_only'] = df['nbr_event'] / df['nbr_event'].sum()
+    df['prob_nevent_only'] = df['nbr_nevent'] / df['nbr_nevent'].sum()
+    df['WoE'] = np.log(df['prob_nevent_only'] / df['prob_event_only'])
+    df['WoE'] = df['WoE'].replace([np.inf, -np.inf], np.nan).fillna(0)
+    df = df.sort_values(['WoE'])
+    df = df.reset_index(drop=True)
+    # Calculates the difference of a Dataframe element compared with another element in the Dataframe (default is element in previous row)
+    df['diff_prob_event'] = df['prob_event'].diff().abs()
+    df['diff_WoE'] = df['WoE'].diff().abs()
+    df['IV'] = (df['prob_nevent_only'] - df['prob_event_only']) * df['WoE']
+
+    return df
+
+def bar_by_woe(df_WoE, x_rotation=0):
+    with axes_style({'axes.facecolor': 'gainsboro', 'grid.color': 'white'}):
+        x = np.array(df_WoE.iloc[:, 0].apply(str))
+        y = df_WoE['WoE']
+        
+        mask_y_bz = y < 0
+        mask_y_az = y >= 0
+
+        plt.figure(figsize=(18, 6))
+        plt.bar(x[mask_y_bz], y[mask_y_bz], color="lightcoral")
+        plt.bar(x[mask_y_az], y[mask_y_az], color="steelblue")
+        plt.xlabel(df_WoE.columns[0].replace('_bin',''))
+        plt.ylabel('Weight of Evidence')
+#        plt.title(str('Weight of Evidence by ' + df_WoE.columns[0]))
+        plt.xticks(rotation=x_rotation)
+
+
+def line_by_woe(df_WoE, x_rotation=0):
+    with axes_style({'axes.facecolor': 'gainsboro', 'grid.color': 'white'}):
+        x = np.array(df_WoE.iloc[:, 0].apply(str))
+        y = df_WoE['WoE']
+
+        plt.figure(figsize=(18, 6))
+        plt.plot(x, y, marker='o', linestyle='--', color='steelblue')
+        plt.xlabel(df_WoE.columns[0].replace('_bin',''))
+        plt.ylabel('Weight of Evidence')
+#        plt.title(str('Weight of Evidence by ' + df_WoE.columns[0]))
+        plt.xticks(rotation=x_rotation)
